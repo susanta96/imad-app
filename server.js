42ab5444a6fb93156bfa7d3acdb89cbf1d84e882
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 21:03:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/submit-name', function (req, res) {//URL: submit-name?name=xxx
 function hash(input, salt){
   //how do we create a hash?  
   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-  return hashed.toString('hex');
+  return ['pbkdf2', "10000", salt, hashed.toString('hex')].join('$');
     
 }
 
