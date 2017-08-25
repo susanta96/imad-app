5a72da45e2342b309e50c8231f358badab1753e5
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 23:19:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,31 @@ app.post('/create-user', function(req,res){
    });
 });
 
+app.post('/login', function(req, res){
+    var username = req.body.username;
+   var password = req.body.password;
+   pool.query('SELECT * from "user" WHERE username = $1',[username],function(err, result){
+       if(err){
+          res.status(500).send(err.toString());
+      }
+      else{
+          if(result.rows.length === 0){
+              res.send(403).send('username/password is invalid');
+          } else{
+              //match the password
+              var dbString=result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt);
+              if(hashedPassword === dbString){
+                  res.send('Credentials correct!!');
+              }
+              else{
+                  res.send(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
 
 var pool= new Pool(config);
 app.get('/test-db',function(req,res){
