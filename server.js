303718e1ebe78cac3cc504365500700b2866b3e2
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 11:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,11 +94,12 @@ app.get('/test-db',function(req,res)){
   pool.query('SELECT * FROM test',function(err, result)){
       if(err){
           res.status(500).send(err.toString());
-      }else{
+      }
+      else{
           res.send(JSON.stringify(result,rows));
       }
-  });
-});
+  };
+};
 
 app.get('/:articleName',function(req, res) {
     //articleName=article-one
