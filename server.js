8ca13f6322b5071ac8421774b8c5c9d5c0693963
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 22:06:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/hash/:input',function(req,res){
    res.send(hashedString);
 });
 
-app.post('create-user', function(req,res){
+app.post('/create-user', function(req,res){
    //username,password
    //JSON
    var username = req.body.username;
