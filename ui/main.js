4795f6031c9c45f977817a81cb476ececd3f709d
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 23:40:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,14 +10,11 @@ submit.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                  list += '<li>' + names[i] +'</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML=list;
+                alert('Login Successfully!!');
+            }else if(request.status === 403){
+                alert('Username/Password is Invalid');
+            } else if(request.status === 500){
+                alert('Something went wrong in the server');
             }
         }  
         //not done yet
