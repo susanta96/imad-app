dca1af058e0a0a3b031f5b5263a8c0f6ab5dccf8
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-08-25 12:19:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/articles/:articleName',function(req, res) {
     //articles[articleName]=={} content obj of article One
     var articleName = req.params.articleName;
     
-    pool.query("SELECT * FROM article WHERE title=" .req.params.articleName ,function(err,result){
+    pool.query("SELECT * FROM article WHERE title=" +req.params.articleName ,function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else{
