39017ccf47a33fca1a0f787de30fe78ea1c6c03d
Susanta Chakraborty <susant.vanu7278@gmail.com>
2017-09-05 23:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -189,24 +189,20 @@ app.post('/submit-comment/:articleName', function (req, res) {
         res.status(403).send('Only logged in users can comment');
     }
 });
-
-app.get('/articles/:articleName',function(req, res) {
-    //articleName=article-one
-    //articles[articleName]=={} content obj of article One
-    var articleName = req.params.articleName;
-    
-    pool.query("SELECT * FROM article WHERE title=$1", [req.params.articleName] ,function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        } else{
-            if(result.rows.length === 0){
-                res.status(404).send('Article Not Found!!');
-            } else{
-                var articleData = result.rows[0];
-                res.send(createTemplate(articleData)); 
-            }
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
         }
-   });
+    }
+  });
 });
 
 app.get('/ui/:fileName', function (req, res) {
